5d3256a2eb233ea4887f344ae671082096ddf098
ap-limon <jnsapl5@gmail.com>
2022-03-08 23:21:37 +0600

    updated the searchbar and Card

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import Filter from "./components/Filter/Filter";
 import Navbar from "./components/Navbar/Navbar";
 import Pagination from "./components/Pagination/Pagination";
 import Search from "./components/Search/Search";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let api = `https://rickandmortyapi.com/api/character/?page=1`;
+  let [fetchData, updateFetchData] = useState([]);
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+
+  let { info, results } = fetchData;
   useEffect(() => {
     (async function () {
       let data = await fetch(api).then(res => res.json());
-      console.log(data);
+      updateFetchData(data);
     })();
   }, [api]);
   return (
@@ -22,12 +26,14 @@ function App() {
       <h1 className="text-center mb-3">
         Characters
       </h1>
+      <Search
+        setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
         <div className="row">
           Filter components will be placed here
           <div className="col-lg-8 col-12">
             <div className="row">
-              Card components will be placed here.
+              <Card results={results} />
             </div>
           </div>
         </div>
